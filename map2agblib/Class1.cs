3b12c86883c7cd64f62e866671d8aa1139347955
StarGate01 <christoph.honal@web.de>
2017-02-21 16:20:16 +0100

    Project init

---
+++ map2agblib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace map2agblib
+{
+    public class Class1
+    {
+    }
+}
