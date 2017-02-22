221881c3212b00f6150fc2ae310d995ec1978f8f
SBird1337 <auerphilipp38@yahoo.de>
2017-02-22 15:13:29 +0100

    more documentation

--- map2agblib/Data/MapNameTable.cs
+++ map2agblib/Data/MapNameTable.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace map2agblib.Data
 {
+    /// <summary>
+    /// Represents the MapNameTable to read and write names in the project
+    /// </summary>
     public class MapNameTable
     {
+        #region Constances
         private const int MAX_NAME = 0x6C;
         private const int MAX_CHARS = 16;
-        private string[] _names;
+        #endregion
 
+        #region Fields
+        private string[] _names;
+        #endregion
+
+        #region Indexed Properties
+        /// <summary>
+        /// Gets or sets a name of the Table. Throws IndexOutOfRangeException if index is not in range. Throws InvalidOperationException if the string is too long.
+        /// </summary>
+        /// <param name="index">Index of the Name</param>
+        /// <returns></returns>
         public string this[int index]
         {
             get
@@ -29,10 +43,16 @@ namespace map2agblib.Data
                 _names[index] = value;
             }
         }
+        #endregion
 
+        #region Constructor
+        /// <summary>
+        /// Creates a new default MapNameTable and fills it with DUMMY_NAME entries
+        /// </summary>
         public MapNameTable()
         {
             _names = Enumerable.Repeat("DUMMY_NAME", MAX_NAME).ToArray();
         }
+        #endregion
     }
 }
