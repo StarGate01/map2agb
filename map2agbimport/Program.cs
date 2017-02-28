88aae57f0fd6cef465a7452247706106ade3f530
SBird1337 <auerphilipp38@yahoo.de>
2017-02-28 19:49:36 +0100

    create import project

---
+++ map2agbimport/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace map2agbimport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options opt = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, opt))
+            {
+                long mapTable = -1;
+                long nameTable = -1;
+                Console.WriteLine(mapTable);
+                /* do stuff */
+            }
+            Console.ReadKey();
+            /* don't do stuff */
+        }
+    }
+}
