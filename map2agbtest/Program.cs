2a4fdab9b51b802e57ba4cf4495ea8e4edc185ac
StarGate01 <christoph.honal@web.de>
2017-02-28 16:40:04 +0100

    test project added

---
+++ map2agbtest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace map2agbtest
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello World");
+
+        }
+
+    }
+
+}
