5401e2c32f4b0e91bf013d32376bc4a8d91d7d08
SBird1337 <auerphilipp38@yahoo.de>
2017-03-02 15:23:32 +0100

    import mapheader

---
+++ map2agbimport/Graphics/Tile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace map2agbimport.Graphics
+{
+    public class Tile
+    {
+        #region Fields
+
+        private byte[] _indices;
+
+        #endregion
+
+        #region Properties
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index >= 32)
+                    throw new ArgumentOutOfRangeException("index");
+                return _indices[index];
+            }
+
+            set
+            {
+                if (index >= 32)
+                    throw new ArgumentOutOfRangeException("index");
+                if (value >= 16)
+                    throw new ArgumentOutOfRangeException("value");
+                _indices[index] = value;
+            }
+        }
+
+        #endregion
+    }
+}
--- map2agbimport/Program.cs
+++ map2agbimport/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Globalization;
 using map2agblib.Common;
 using System.IO;
+using map2agblib.Map;
 
 namespace map2agbimport
 {
@@ -28,10 +26,77 @@ namespace map2agbimport
                     {
                         fs = null;
                         /*TODO: Start Reading the object model */
+                        if (opt.ExportMap || opt.ExportTileset)
+                        {
+                            if (mapTable == -1)
+                            {
+                                Console.Error.WriteLine("error: cannot import map without --maptable option specified");
+                                return;
+                            }
+                            br.BaseStream.Seek(mapTable + (4 * opt.BankNumber), SeekOrigin.Begin);
+                            br.BaseStream.Seek((br.ReadUInt32() & 0x1FFFFFF) + (4 * opt.MapNumber), SeekOrigin.Begin);
+                            br.BaseStream.Seek(br.ReadUInt32() & 0x1FFFFFF, SeekOrigin.Begin);
+                            uint mapFooterOffset = br.ReadUInt32();
+                            uint eventOffset = br.ReadUInt32();
+                            uint levelScriptOffset = br.ReadUInt32();
+                            uint connectionOffset = br.ReadUInt32();
+
+                            MapHeader header = new MapHeader()
+                            {
+                                Music = br.ReadUInt16(),
+                                Index = br.ReadUInt16(),
+                                Name = br.ReadByte(),
+                                Flash = br.ReadByte(),
+                                Weather = br.ReadByte(),
+                                Type = br.ReadByte(),
+                                Unknown = br.ReadUInt16(),
+                                ShowName = br.ReadByte(),
+                                BattleStyle = br.ReadByte()
+                            };
+
+                            br.BaseStream.Seek(mapFooterOffset & 0x1FFFFFF, SeekOrigin.Begin);
+                            MapFooter footer = new MapFooter();
+                            footer.Width = br.ReadUInt32();
+                            footer.Height = br.ReadUInt32();
+                            uint borderOffset = br.ReadUInt32();
+                            uint mapDataOffset = br.ReadUInt32();
+                            uint blocksetOneOffset = br.ReadUInt32();
+                            uint blocksetTwoOffset = br.ReadUInt32();
+                            byte borderWidth = br.ReadByte();
+                            byte borderHeight = br.ReadByte();
+                            ushort padding = br.ReadUInt16();
+
+                            br.BaseStream.Seek(borderOffset & 0x1FFFFFF, SeekOrigin.Begin);
+
+                            ushort[][] borderBlock = Enumerable.Repeat(new ushort[borderWidth], borderHeight).ToArray();                            
+                            for (int y = 0; y < borderHeight; ++y)
+                                for (int x = 0; x < borderWidth; ++x)
+                                    borderBlock[y][x] = br.ReadUInt16();
+
+                            ushort[][] mapBlock = Enumerable.Repeat(new ushort[footer.Width], (int)footer.Height).ToArray();
+                            for (int y = 0; y < footer.Height; ++y)
+                                for (int x = 0; x < footer.Width; ++x)
+                                    mapBlock[y][x] = br.ReadUInt16();
+
+                            footer.FirstTilesetID = "tsp_m_" + opt.BankNumber.ToString() + "_" + opt.MapNumber.ToString();
+                            footer.SecondTilesetID = "tss_m_" + opt.BankNumber.ToString() + "_" + opt.MapNumber.ToString();
+                            footer.BorderBlock = borderBlock;
+                            footer.MapBlock = mapBlock;
+                            footer.BorderWidth = borderWidth;
+                            footer.BorderHeight = borderHeight;
+
+                            header.Footer = footer;
+                            header.Events = null;
+                            header.Connections = null;
+                            header.MapScripts = null;
+                        }
+
+
                     }
                 }
                 catch (Exception ex)
                 {
+                    Console.Error.WriteLine("error: {0}", ex.Message);
                     error = true;
                 }
                 finally
