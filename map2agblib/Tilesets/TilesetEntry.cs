ecc7b31812c7699be04212f7b9a6ac7d55e5d7e1
StarGate01 <christoph.honal@web.de>
2017-03-05 07:07:49 +0100

    Datamodel tilemap fix

--- map2agblib/Tilesets/TilesetEntry.cs
+++ map2agblib/Tilesets/TilesetEntry.cs
@@ -15,21 +15,27 @@ namespace map2agblib.Tilesets
         [DataMember]
         public BlockBehaviour Behaviour { get; set; }
 
+        /// <summary>
+        /// The Tilemap entries
+        /// Order: bottom-layer (left-top, right-top, left-bottom, right-bottom) and the same for the top layer
+        /// </summary>
         [DataMember]
-        public BlockTilemap TilemapEntry { get; set; }
+        public BlockTilemap[] TilemapEntry { get; set; }
 
         #endregion
 
         #region Construcotrs
 
-        public TilesetEntry(BlockBehaviour behaviour, BlockTilemap tilemap)
+        public TilesetEntry(BlockBehaviour behaviour, BlockTilemap[] tilemap)
         {
             Behaviour = behaviour;
             TilemapEntry = tilemap;
         }
 
         public TilesetEntry()
-        { }
+        {
+            TilemapEntry = Enumerable.Repeat(new BlockTilemap(), 8).ToArray();
+        }
 
         #endregion
 
